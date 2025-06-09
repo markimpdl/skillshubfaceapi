fbc9ab0c8ada2ed4d9df9cc35805d5145cf98155
MARCOS SOARES <marcos.soares@emiratesskills.ae>
2025-06-09 14:08:18 +0400

    Services

---
+++ FaceApi/FaceApi/Services/SchoolService.cs
@@ -0,0 +1,24 @@
+﻿using FaceApi.Data;
+using FaceApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaceApi.Services
+{
+    public class SchoolService
+    {
+        private readonly ApiDbContext _db;
+        public SchoolService(ApiDbContext db) => _db = db;
+
+        public async Task<School> CreateAsync(string name)
+        {
+            var school = new School { Name = name};
+            _db.Schools.Add(school);
+            await _db.SaveChangesAsync();
+            return school;
+        }
+
+        public async Task<List<School>> GetAllAsync() => await _db.Schools.ToListAsync();
+        public async Task<School> GetByIdAsync(int id) => await _db.Schools.FindAsync(id);
+
+    }
+}
